c24b7755e6f7345d0fe6bd107249891ea609aad5
yihnatchuk <yihnatchuk@griddynamics.com>
2022-07-02 02:38:31 +0300

    lecture5

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,8 +1,24 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
+// Union type
+// Literal types
+// Aliases types
+
+type Combine = number | string;
+type ResultDescription = 'as number' | 'as text'
+
+function combine(n1: Combine, n2: Combine, resultType: ResultDescription) {
+    let res;
+
+    if (typeof n1 === "number" && typeof n2 === "number" || resultType === 'as number') {
+        res = +n1 + +n2;
+    } else {
+        res = n1.toString() + n2.toString();
+    }
+
+    return res;  
 }
 
-const num1 = 5;
-const num2 = 10.3;
+console.log(combine(20, 25, 'as number'));
 
-console.log(add(num1, num2));
+console.log(combine('20', '25', 'as number'));
+
+console.log(combine('Max', 'Ana', 'as text'));
